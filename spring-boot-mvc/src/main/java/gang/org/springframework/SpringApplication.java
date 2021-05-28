4cfc2580d9ce2ceebf5f133b54d6db42df6b6b53
ChenGang <chengang@icolor.com.cn>
2021-05-28 21:13:55 +0800

    spring mvc 简单实践

--- spring-boot-mvc/src/main/java/gang/org/springframework/SpringApplication.java
+++ spring-boot-mvc/src/main/java/gang/org/springframework/SpringApplication.java
@@ -1,8 +1,11 @@
 package gang.org.springframework;
 
-import org.apache.catalina.Context;
-import org.apache.catalina.LifecycleException;
-import org.apache.catalina.LifecycleListener;
+import gang.org.springframework.servlet.DispatcherServlet;
+import org.apache.catalina.*;
+import org.apache.catalina.connector.Connector;
+import org.apache.catalina.core.StandardContext;
+import org.apache.catalina.core.StandardEngine;
+import org.apache.catalina.core.StandardHost;
 import org.apache.catalina.startup.Tomcat;
 
 /**
@@ -15,12 +18,53 @@ public class SpringApplication {
 
     public static void run(Class<?> primarySource, String... args){
         tomcatOn();
+        startTomcat();
+    }
+
+    public static void startTomcat() {
+        Tomcat tomcat = new Tomcat();
+
+        Server server = tomcat.getServer();
+        Service service = server.findService("Tomcat");
+
+        Connector connector = new Connector();
+        connector.setPort(8080);
+
+        StandardEngine engine = new StandardEngine();
+        engine.setDefaultHost("localhost");
+
+        StandardHost host = new StandardHost();
+        host.setName("localhost");
+
+        String contextPath = "";
+        StandardContext context = new StandardContext();
+        context.setPath(contextPath);
+        context.addLifecycleListener(new Tomcat.FixContextListener());
+
+
+
+        host.addChild(context);
+        engine.addChild(host);
+
+        service.setContainer(engine);
+        service.addConnector(connector);
+        tomcat.addServlet(contextPath,"dispatcher",new DispatcherServlet());
+        context.addServletMappingDecoded("/*","dispatcher");
+
+        try{
+            tomcat.start();
+            tomcat.getServer().await();
+        }
+        catch (LifecycleException e){
+            e.printStackTrace();
+        }
     }
 
     public static void tomcatOn(){
         Tomcat tomcat = new Tomcat();
         tomcat.setPort(8080);
-        Context context = tomcat.addContext("/", System.getProperty("java.io.tmpdir"));
+
+        Context context = tomcat.addContext("", "D:\\work\\learn\\gang-mars-java\\docBase");
         try {
             context.addLifecycleListener((LifecycleListener) Class.forName(tomcat.getHost().getConfigClass()).newInstance());
             tomcat.start();

1d10f106aa3d2db42b72e61fc53befff852c1220
ChenGang <chengang@icolor.com.cn>
2021-05-28 09:05:49 +0800

    spring mvc 简单实践

--- spring-boot-mvc/src/main/java/gang/org/springframework/servlet/HttpServletBean.java
+++ spring-boot-mvc/src/main/java/gang/org/springframework/servlet/HttpServletBean.java
@@ -1,9 +1,11 @@
 package gang.org.springframework.servlet;
 
+import javax.servlet.http.HttpServlet;
+
 /**
  * @author gang.chen
  * @description
  * @time 2021/5/28 8:37
  */
-public class HttpServletBean {
+public class HttpServletBean extends HttpServlet {
 }
